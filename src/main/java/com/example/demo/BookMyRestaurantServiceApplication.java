b5b71ed91e8b43bc8406797747065797fae1d07d
Ayushkuruvilla <ayush.kuruvilla@gmail.com>
2022-04-28 11:05:34 +0530

    changes

---
+++ src/main/java/com/example/demo/BookMyRestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyRestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyRestaurantServiceApplication.class, args);
+	}
+
+}
